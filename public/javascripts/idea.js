acc42e8edeee83a9230d0d412b8d0e43674f3f1c
Jeff Schoolcraft <jeff@thequeue.net>
2010-04-09 07:14:44 -0400

    working on the slider

--- public/javascripts/idea.js
+++ public/javascripts/idea.js
@@ -3,10 +3,10 @@ $(function() {
 		min: 1,
 		max: 10,
 		slide: function(event, ui) {
-			
+			input_id = "#idea_" + this.id.replace("-slider", "");
+			console.log(input_id);
+			$(input_id).val(ui.value);
+			console.log(ui.value);
 		}
 	});
-	// somehow find the id of the slider div, chop off -slider, use the remaining as id
-	// use $("#id").value to set value initially
-	// use $("#id") in the after function to set the value of the slider
 });
