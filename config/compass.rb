29c3da8ef54ecb8399e57909e1e176a93f17bd13
Jeff Schoolcraft <jeff@thequeue.net>
2010-04-09 00:14:58 -0400

    one more try

--- config/compass.rb
+++ config/compass.rb
@@ -10,3 +10,4 @@ environment = Compass::AppIntegration::Rails.env
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 # Sass::Plugin.options[:template_location] = sass_dir
+Sass::Plugin.options[:template_location] = { sass_dir => 'public/stylesheets' }
