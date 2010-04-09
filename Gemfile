2553343616f611d5526a5b77c99e148c8966d271
Jeff Schoolcraft <jeff@thequeue.net>
2010-04-08 22:42:48 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'http://rubygems.org'
+
+group :default do
+  gem 'rails', '3.0.0.beta2'
+  gem 'pg'
+  gem 'haml-edge', :require => 'haml'
+  gem 'inherited_resources'
+  gem 'devise', '1.1.rc0'
+end
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# Bundle gems for certain environments:
+# gem 'rspec', :group => :test
+# group :test do
+#   gem 'webrat'
+# end
---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.cookie_secret = '5a74627270c9d678653704105182dace68391e1bda13e43eb2abd4b9cd12dee5982a237897d99b273bb435fe132bbd0145ef8ed629a854b759b8bd3d9bee15b2'
