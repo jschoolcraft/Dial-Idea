49acba3dfcb56dee7282d3e343c5cef6348d0d39
Jeff Schoolcraft <jeff@thequeue.net>
2010-04-09 17:07:03 -0400

    a bit of validation

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,11 @@
 class Idea < ActiveRecord::Base
+  validates_presence_of :title
+  validates_presence_of :pain_reliever
+  validates_presence_of :money_multiplier
+  validates_numericality_of :demand
+  validates_numericality_of :cost
+  validates_numericality_of :frequency
+  validates_numericality_of :revenue
   validates_inclusion_of :demand,    :in => 1..10
   validates_inclusion_of :cost,      :in => 1..10
   validates_inclusion_of :frequency, :in => 1..10
