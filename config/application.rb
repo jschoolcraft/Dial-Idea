f0743ff2d15aeb211b2c07fbca32d495c936ede0
Jeff Schoolcraft <jeff@thequeue.net>
2010-04-09 01:18:52 -0400

    fuck it all

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'hassle'
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
@@ -40,9 +39,5 @@ module Dialidea
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters << :password
-  end
-  
-  class HassleRailtie < Rails::Railtie  
-    config.middleware.use Hassle if Rails.env == 'production'   
   end
 end
